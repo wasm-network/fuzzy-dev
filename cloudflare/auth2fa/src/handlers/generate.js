eee4e07bb72a9b541d26f053732488a443277bae
Hugh Lang <hughlang@gmail.com>
2019-08-26 14:07:50 -0400

    Connect page checklist ok. Scan page started

--- cloudflare/auth2fa/src/handlers/generate.js
+++ cloudflare/auth2fa/src/handlers/generate.js
@@ -13,12 +13,17 @@ const Speakeasy = require("speakeasy");
 
 export default async request => {
     try {
+        let url = new URL(request.url);
+        let params = url.searchParams;
+        let addr = params.get('id').trim();
         var options = {length: 32, name: 'Fuzzy Dice'};
         var secret = Speakeasy.generateSecret(options);
+
+        await FZData.put(addr, secret.base32);
         const headers = { 'Content-Type': 'image/png' };
         const qr_png = qr.imageSync( secret.base32 );
         return new Response(qr_png, { headers });
     } catch (err) {
-        return new Response("Unknown error");
+        return new Response(err)
     }
 }
--- website/assets/js/app/login.js
+++ website/assets/js/app/login.js
@@ -1,6 +1,7 @@
 /// Javascript for login page
 FZD.Auth = {
     status: "",
+    eth_address: "",
     authAPI: axios.create({
         baseURL: "http://127.0.0.1:5000/",
         timeout: 1000
@@ -27,6 +28,34 @@ FZD.Auth = {
     /// Call this function after Web3 account is acquired (Metamask is logged in)
     checkMetamask: function(addr) {
         window.location = "/login/metamask";
+    },
+    startScan: function() {
+        window.location = "/login/scan";
+    },
+    fetchQR: function() {
+        if (FZD.Auth.eth_address.length == 0) {
+            FZD.log("Eth address not set. Redirect to connect page");
+            document.getElementById("scan-panel").style.display = "none";
+            document.getElementById("no-scan").style.display = "block";
+            // window.location = "/login/connect";
+        } else {
+            FZD.Auth.authAPI.get("/generate?id=" + FZD.Auth.eth_address)
+            .then(function (response) {
+                // FZD.log("Result: " + response.data.result);
+                // let result = response.data.result;
+                // if (result) {
+                //     FZD.log("Hey you have an account already!");
+                //     let data = response.data.data;
+                //     if (data != null) {
+                //         FZD.log("You have data. Let's use it");
+                //         window.location = "/home";
+                //     }
+                // } else {
+                //     FZD.log("You don't have an account.");
+                //     window.location = "/login/connect";
+                // }
+            });
+        }
     },
     setup: function() {
         FZD.log("setup");
